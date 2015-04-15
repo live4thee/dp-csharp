e69c9c8c9a0cb0b872d864fb607dc98c147b01b2
Li Qun <live4thee@gmail.com>
2015-04-15 14:18:33 +0800

    Add a retry sample from stack overflow.

---
+++ dp-csharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DpCSharp
+{
+    class Program
+    {
+        class WillFailAtFirstTime
+        {
+            int count = 0;
+
+            public T Do<T>(Func<T> func)
+            {
+                if (count++ == 0)
+                {
+                    throw new Exception("fault at first try");
+                }
+
+                return func();
+            }
+        }
+
+        static void testRetry()
+        {
+            var v = new WillFailAtFirstTime();
+            Func<int> f = () => v.Do<int>(() => { return 1; });
+
+            int i = Retry.Do<int>(f, TimeSpan.FromSeconds(1), 2);
+            Console.WriteLine("Retry result: " + i);
+        }
+
+        static void Main(string[] args)
+        {
+            testRetry();
+        }
+    }
+}
---
+++ dp-csharp/Retry.cs
@@ -0,0 +1,48 @@
+﻿namespace DpCSharp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+
+    /// <summary>
+    /// A class to encapsulate retry logic, code from:
+    /// http://stackoverflow.com/questions/1563191/c-sharp-cleanest-way-to-write-retry-logic 
+    /// </summary>
+    public static class Retry
+    {
+        public static void Do(
+            Action action,
+            TimeSpan retryInterval,
+            int retryCount = 3)
+        {
+            Do<object>(() =>
+            {
+                action();
+                return null;
+            }, retryInterval, retryCount);
+        }
+
+        public static T Do<T>(
+            Func<T> action,
+            TimeSpan retryInterval,
+            int retryCount = 3)
+        {
+            var exceptions = new List<Exception>();
+
+            for (int retry = 0; retry < retryCount; retry++)
+            {
+                try
+                {
+                    return action();
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                    Thread.Sleep(retryInterval);
+                }
+            }
+
+            throw new AggregateException(exceptions);
+        }
+    }
+}
